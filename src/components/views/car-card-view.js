07e8f604fa5281ffae60a049bfa53c49c111ef51
leobrossault <brossault.leo@gmail.com>
2019-05-26 23:41:50 +0200

    Level 4 - Add degressive price for each car according to the duration of the rent

--- src/components/views/car-card-view.js
+++ src/components/views/car-card-view.js
@@ -11,6 +11,15 @@ const mapStateToProps = state => {
   return { filters: state.filters };
 };
 
+/*
+ * Percentages values according to the duration
+ */
+const PERCENTAGES_VALUES = {
+  1: 10,
+  4: 30,
+  10: 50
+};
+
 class CarCardView extends Component {
   constructor() {
     super();
@@ -37,14 +46,51 @@ class CarCardView extends Component {
     /*
      * Sum distance price and time price for the total
      */
-    const totalPice = (distancePrice + timePrice) / 100;
+    const totalPrice = (distancePrice + timePrice) / 100;
+
+    /*
+     * Return the total price, with degressive price according to the duration
+     */
+    const degressivePrice = this.props.filters.duration
+      ? this.getDegressivePrice(this.props.filters.duration.value, totalPrice)
+      : totalPrice;
 
     /*
      * Check decimal count to beautify the render
      */
-    return decimalCount(totalPice) > 0
-      ? parseFloat(totalPice).toFixed(1)
-      : totalPice;
+    return decimalCount(degressivePrice) > 0
+      ? parseFloat(degressivePrice).toFixed(1)
+      : degressivePrice;
+  }
+
+  /*
+   * Calculate degressivity on the price
+   */
+  getDegressivePrice(duration, price) {
+    let percentage = 0;
+
+    for (let durationStep in PERCENTAGES_VALUES) {
+      if (parseInt(durationStep) < duration) {
+        percentage = PERCENTAGES_VALUES[durationStep];
+      }
+    }
+
+    return percentage ? price : price - price * (percentage / 100);
+  }
+
+  /*
+   * Calculate total price per day
+   */
+  getTotalPricePerDay(totalPrice, duration) {
+    if (!duration) {
+      return totalPrice;
+    }
+
+    const totalPricePerDay = totalPrice / duration.value;
+
+    return decimalCount(totalPricePerDay) > 0
+      ? parseFloat(totalPricePerDay).toFixed(1)
+      : totalPricePerDay;
   }
 
   render() {
@@ -58,8 +104,25 @@ class CarCardView extends Component {
           </p>
 
           <div class="car-card-view__infos__price">
-            <span>{this.getRentalPrice()}</span>
-            <span>€</span>
+            <div class="car-card-view__infos__price--day">
+              <div>
+                {/* Price  per day */}
+                <span>
+                  {this.getTotalPricePerDay(
+                    this.getRentalPrice(),
+                    this.props.filters.duration
+                  )}
+                </span>
+                <span>€</span>
+              </div>
+
+              <p>/day</p>
+            </div>
+
+            {/* Total price */}
+            <div class="car-card-view__infos__price--total">
+              total : {this.getRentalPrice()}€
+            </div>
           </div>
         </div>
       </div>
